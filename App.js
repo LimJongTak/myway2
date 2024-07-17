8bcaf2b71f52692f4b2f20d340b766d7b8af6dbe
LimJongTak <39991534+LimJongTak@users.noreply.github.com>
2024-07-18 00:02:42 +0900

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import CommunityPage from "./CommunityPage";
+import StartPage from "./StartPage"; // Import the StartPage component
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/community" element={<CommunityPage />} />
+      <Route path="/start" element={<StartPage />} /> {/* Add this line */}
     </Routes>
   );
 }
